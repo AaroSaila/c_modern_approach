a87c72190fb04052e320a8ea31e6adcfb575f65f
Aaro Saila <aaros.saila@gmail.com>
2024-05-21 17:02:04 +0300

    progress

---
+++ ch3/exercises/ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  float x;
+  int j;
+
+  scanf(" %d %f%d", &i, &x, &j);
+
+  printf("i = %d\nx = %f\nj = %d\n", i, x, j);
+
+  return 0;
+}
---
+++ ch3/exercises/exercises.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  printf("1.\n");
+  printf("(a)\n");
+  printf("%6d, %4d\n", 86, 1040);
+  printf("(b)\n");
+  printf("%12.5e\n", 30.253);
+  printf("(c)\n");
+  printf("%.4f\n", 83.162);
+  printf("(d)\n");
+  printf("%-6.2g", .0000009979);
+  return 0;
+}
