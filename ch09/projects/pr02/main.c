d55a97de6a516137bd8e531d6690e15b361392fc
Aaro Saila <aaros.saila@gmail.com>
2024-08-10 12:47:53 +0300

    ch09 projects progress

---
+++ ch09/projects/pr02/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+float calculate_taxes(float income) {
+  float taxes;
+
+  if (income < 750) {
+    taxes = income * 0.01f;
+  } else if (income <= 2250) {
+    taxes = 7.5f + 0.02f * (income - 750.0f);
+  } else if (income <= 3750) {
+    taxes = 37.5f + 0.03f * (income - 2250.0f);
+  } else if (income <= 5250) {
+    taxes = 82.5f + 0.04f * (income - 3750.0f);
+  } else if (income <= 7000) {
+    taxes = 142.5f + 0.05f * (income - 5250.0f);
+  } else {
+    taxes = 230.0f + 0.06f * (income - 7000.0f);
+  }
+
+  return taxes;
+}
+
+int main() {
+  float income;
+  printf("Enter amount of taxable income: ");
+  scanf("%f", &income);
+
+  float taxes = calculate_taxes(income);
+
+  printf("Taxes due: $%.2f\n", taxes);
+  
+  return 0;
+}
